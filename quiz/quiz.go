302f0cb68972a2ffad07203ef43398444c0d4411
ahnesther <52338697+ahnesther@users.noreply.github.com>
2022-03-15 23:23:48 -0400

    Had to look around to implement the timer because I don't know much about goroutines and concurrency

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -5,20 +5,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+	"time"
 )
+
+/* Learned how to:
+- Use a basic goroutine: select/go (some minor concurrency)
+- Parse CLI arguments
+- Open/read/parse csv files
+- Go label, but ymm
+- TODO: what is a go closure.
+- TODO: I could refactor
+*/
 
 func main() {
 	// parse flag names
-	csvFilename := flag.String("csv", "problems.csv",
-		"csv file in format of qna")
-	// solveTime := flag.Float64("time", 3.0, "time limit to solve problems")
-	numberTries := flag.Int("tries", 1, "number of tries to correctly a question")
-
-	if *numberTries == 0 {
-		*numberTries = 1 // set it to default!!! if 0!!
-	}
-
+	csvFilename := flag.String("csv", "problems.csv", "csv file in format of qna")
+	solveTime := flag.Int("time", 30.0, "time limit to solve the entire quiz in seconds")
 	flag.Parse()
 
 	// open csv then close it
@@ -33,30 +35,39 @@ func main() {
 	csvReader := csv.NewReader(f)
 	data, err := csvReader.ReadAll()
 	if err != nil {
-		fmt.Println("Failed to open csv file: %s\n", csvFilename)
+		fmt.Println("Failed to read csv file: %s\n", csvFilename)
 	}
 
 	// the quiz itself
-	correctAnswers, wrongAnswers := 0, 0
+	correctAnswers := 0
 
+	fmt.Printf("Answer all these questions and you will win (knowledge). \n")
+	quizTimer := time.NewTimer(time.Duration(*solveTime) * time.Second)
+	// Only start the quiz if the user has pressed any key
 	var answer string
+problemLoop:
 	for _, val := range data {
-		i := *numberTries
-		for i > 0 {
-			fmt.Printf("Answer all these questions and you will win: \n")
-			fmt.Println(val[0])
-			fmt.Scanf("%s", &answer)
-			strings.TrimSpace(answer)
+		fmt.Println(val[0])
+		answerChannel := make(chan string)
+		go func() { // scans for an answer
+			fmt.Scanf("%s\n", &answer)
+			answerChannel <- answer // we send the input/answer to the channel.
+		}()
+
+		select {
+		// we remove the default
+		// because even if we run out of time and type an answer,
+		// it is counted as a correct answer (?)
+		case <-quizTimer.C: // if the timer ran out of time.
+			break problemLoop
+		case answer := <-answerChannel: // if we get a call from the answerChannel before the timer
 			if answer == val[1] {
 				correctAnswers++
 				fmt.Println("Woah you guessed right! Very nice. \n")
-				break // to get to the next question; otherwise loops same question.
 			} else {
-				wrongAnswers++
 				fmt.Println("WRONG!! To the gulag with you. \n")
-				i--
 			}
 		}
 	}
-	fmt.Printf("You have answered %d questions correctly and %d wrong! \n", correctAnswers, wrongAnswers)
+	fmt.Printf("You have answered %d questions correctly and %d wrong! \n", correctAnswers, len(data)-correctAnswers)
 }
