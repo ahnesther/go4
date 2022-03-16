4972e31496bb7f905d086a48d2a3a5d7991b16f7
ahnesther <52338697+ahnesther@users.noreply.github.com>
2022-03-15 20:47:14 -0400

    Instead of timer, I used a tries flag, where you have x number of tries per question.

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,10 +13,12 @@ func main() {
 	csvFilename := flag.String("csv", "problems.csv",
 		"csv file in format of qna")
 	// solveTime := flag.Float64("time", 3.0, "time limit to solve problems")
-	numberTries := flag.Int("tries", 1, "number of tries to answer question")
+	numberTries := flag.Int("tries", 1, "number of tries to correctly a question")
+
 	if *numberTries == 0 {
 		*numberTries = 1 // set it to default!!! if 0!!
 	}
+
 	flag.Parse()
 
 	// open csv then close it
@@ -34,15 +37,26 @@ func main() {
 	}
 
 	// the quiz itself
+	correctAnswers, wrongAnswers := 0, 0
+
 	var answer string
 	for _, val := range data {
-		fmt.Printf("Answer all these questions and you will win: \n")
-		fmt.Println(val[0])
-		fmt.Scanf("%s", &answer)
-		if answer == val[1] {
-			fmt.Println("Woah you guessed right! Very nice. \n")
-		} else {
-			fmt.Println("WRONG!! To the gulag with you. \n")
+		i := *numberTries
+		for i > 0 {
+			fmt.Printf("Answer all these questions and you will win: \n")
+			fmt.Println(val[0])
+			fmt.Scanf("%s", &answer)
+			strings.TrimSpace(answer)
+			if answer == val[1] {
+				correctAnswers++
+				fmt.Println("Woah you guessed right! Very nice. \n")
+				break // to get to the next question; otherwise loops same question.
+			} else {
+				wrongAnswers++
+				fmt.Println("WRONG!! To the gulag with you. \n")
+				i--
+			}
 		}
 	}
+	fmt.Printf("You have answered %d questions correctly and %d wrong! \n", correctAnswers, wrongAnswers)
 }
